da4248598e47856a3a5aca9d715237927b59a309
Benjamin Kelly <Benjamin.J.Kelly@live.com>
2020-04-05 16:24:04 -0400

    Create master passwords in GUI

--- password-manager-frontend/src/MasterPasswordCreation.js
+++ password-manager-frontend/src/MasterPasswordCreation.js
@@ -10,8 +10,49 @@ class MasterPasswordCreation extends Component {
         heading: PropTypes.string
     }
 
-    render() {
+    state = {
+        name: "",
+        email: "",
+        pswdN: "",
+        pswdS: ""
+    }
 
+    handleName(e) {
+        this.setState({name: e})
+    }
+
+    handleEmail(e) {
+        this.setState({email: e})
+    }
+
+    handlePswdN(e) {
+        this.setState({pswdN: e})
+    }
+
+    handlePswdS(e) {
+        this.setState({pswdS: e})
+    }
+
+    handleRegister(e) {
+        if(this.state.name.length < 3){
+            alert("The name must be AT LEAST 3 characters")
+            return
+        }
+        if(this.state.email.length < 5){
+            alert("The email must be AT LEAST 5 characters")
+            return
+        }
+        if((this.state.pswdN.length < 5) || (this.state.pswdS.length < 5)){
+            alert("The password length must be AT LEAST 5 characters")
+            return
+        }
+        if(this.state.pswdN != this.state.pswdS){
+            alert("The passwords MUST BE equal")
+            return
+        }
+    }
+
+    render() {
             return (
                 <div className='master-password-creation'>
                     <h2 className="create-password">Create Master Password</h2>
@@ -23,15 +64,28 @@ class MasterPasswordCreation extends Component {
                                         <p className="h5 text-center mb-4">Sign up</p>
                                         <div className="grey-text">
                                             <MDBInput label="Your name" icon="user" group type="text" validate error="wrong"
-                                                      success="right" />
+                                                      success="right"
+                                                      onChange = {(event) =>
+                                                    this.handleName(event.target.value)}
+                                                      value = {this.state.name}/>
                                             <MDBInput label="Your email" icon="envelope" group type="email" validate error="wrong"
-                                                      success="right" />
-                                            <MDBInput label="Confirm your email" icon="exclamation-triangle" group type="text" validate
-                                                      error="wrong" success="right" />
-                                            <MDBInput label="Your password" icon="lock" group type="password" validate />
+                                                      success="right"
+                                                      onChange = {(event) =>
+                                                          this.handleEmail(event.target.value)}
+                                                      value = {this.state.email}/>
+                                            <MDBInput label="Your password" icon="lock" group type="password" validate
+                                                      onChange = {(event) =>
+                                                          this.handlePswdN(event.target.value)}
+                                                      value = {this.state.pswdN}/>
+                                            <MDBInput label="Confirm your password" icon="exclamation-triangle" group type="password" validate
+                                                      error="wrong" success="right"
+                                                      onChange = {(event) =>
+                                                          this.handlePswdS(event.target.value)}
+                                                      value = {this.state.pswdS}/>
                                         </div>
                                         <div className="text-center">
-                                            <MDBBtn color="primary">Register</MDBBtn>
+                                            <MDBBtn onClick= {(event) =>
+                                                this.handleRegister(event.target.value)} color="primary">Register</MDBBtn>
                                         </div>
                                     </form>
                                 </MDBCol>
