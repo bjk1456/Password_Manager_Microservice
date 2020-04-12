950462bcb2f180457beaf973df7f0b34f2cb71c0
Benjamin Kelly <Benjamin.J.Kelly@live.com>
2020-04-12 14:00:49 -0400

    Use Postgres function authenticate_email to authenticate the user

---
+++ password-manager-restapi-user/src/controllers/v0/postgres.router.ts
@@ -0,0 +1,32 @@
+import { Router, Request, Response } from 'express';
+import { UserRouter } from './users/routes/user.router';
+const { Pool, Client } = require('pg')
+
+const pool = new Pool({
+    user: process.env.POSTGRESS_USERNAME,
+    host: process.env.POSTGRESS_HOST,
+    database: process.env.POSTGRESS_DB,
+    password: process.env.POSTGRESS_PASSWORD,
+    port: 5432,
+})
+
+const router: Router = Router();
+
+router.use('/users', UserRouter);
+
+router.get('/', async (req: Request, res: Response) => {
+    const request = {
+        text: 'CALL store_master_password($1, $2);',
+        values: ['Jeb@Microsoft.com','JebBush']
+    }
+    await pool.query(request).then((body)  => {
+        res.send(`Successfully added master password`);
+    }).catch((err) => {
+        res.send(err.detail);
+        console.log(`the error is ${err.detail}`);
+    })
+    console.log("Sup dude?")
+
+});
+
+export const PostgresRouter: Router = router;
--- password_manager.sql
+++ password_manager.sql
@@ -1,16 +1,16 @@
 DROP TABLE IF EXISTS master_password cascade;
-CREATE TABLE IF NOT EXISTS master_password ( master_p_id serial primary key, email text not null unique, m_password text not null);
+CREATE TABLE IF NOT EXISTS master_password ( master_p_id serial primary key, email text not null unique, m_password text not null, created_at time with time zone);
 
 DROP TABLE IF EXISTS regular_password cascade;
 CREATE TABLE IF NOT EXISTS regular_password ( regular_p_id serial primary key, r_password text not null, 
-	master_p_id serial references master_password(master_p_id));
+	master_p_id serial references master_password(master_p_id), created_at time with time zone);
 
 CREATE OR REPLACE PROCEDURE store_master_password(_master_email TEXT, _master_password TEXT)
 LANGUAGE plpgsql    
 AS $$
 BEGIN
-    INSERT INTO master_password (email, m_password) 
-    VALUES ( _master_email, crypt(_master_password, gen_salt('bf')) );
+    INSERT INTO master_password (email, m_password, created_at) 
+    VALUES ( _master_email, crypt(_master_password, gen_salt('bf')), now() );
 
     COMMIT;
 END;
@@ -29,13 +29,12 @@ AS $$
 DECLARE
 	m_id real := (SELECT master_p_id FROM master_password WHERE master_password.email = _master_email);
 BEGIN
-    INSERT INTO regular_password (r_password, master_p_id)
-    VALUES (PGP_SYM_ENCRYPT(_regular_password, _key)::text, m_id );
+    INSERT INTO regular_password (r_password, master_p_id, created_at)
+    VALUES (PGP_SYM_ENCRYPT(_regular_password, _key)::text, m_id, now() );
 
     COMMIT;
 END;
 $$;
-
 
 CREATE OR REPLACE FUNCTION get_regular_password(_master_email text, _regular_password text) RETURNS TABLE(
     freetext_password text)
